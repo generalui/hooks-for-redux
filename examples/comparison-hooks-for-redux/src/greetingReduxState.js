5548091d5e9096f11b44b6038788faa66f0ff390
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2019-10-18 16:24:08 -0700

    OOO! Improved the hooks-for-redux comparison code

--- examples/comparison-hooks-for-redux/src/greetingReduxState.js
+++ examples/comparison-hooks-for-redux/src/greetingReduxState.js
@@ -2,12 +2,10 @@ import { useReduxState } from 'hooks-for-redux'
 
 const DEFAULT_GREETING = "hello, hooks-for-redux!";
 
-const [useGreetingSubscription, /*updateGreeting*/, addReducers] =
+export const [useGreetingSubscription, /*updateGreeting*/, addReducers] =
   useReduxState('greeting', DEFAULT_GREETING);
 
-const {resetGreeting, setGreeting} = addReducers({
+export const {resetGreeting, setGreeting} = addReducers({
   resetGreeting: () => DEFAULT_GREETING,
   setGreeting: (store, greeting) => greeting
 })
-
-export {useGreetingSubscription, resetGreeting, setGreeting}
--- examples/comparison-hooks-for-redux/src/index.js
+++ examples/comparison-hooks-for-redux/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Provider} from 'hooks-for-redux'
+import { Provider } from 'hooks-for-redux'
 import './index.css'
 import App from './App'
 
