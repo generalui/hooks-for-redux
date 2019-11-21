ab6d327bd85bbe5ed5bdde7fe965e42961f9d01f
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2019-11-21 15:55:14 -0800

    new example

---
+++ examples/tiny-toggle/src/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunkMiddleware from "redux-thunk";
+import { Provider, useRedux, createStore, setStore } from "hooks-for-redux";
+
+// store.js
+const store = setStore(
+  createStore({}, composeWithDevTools(applyMiddleware(thunkMiddleware)))
+);
+
+// toggleState.js
+const [useToggle, { toggleSwitch }] = useRedux("toggle", false, {
+  toggleSwitch: state => !state
+});
+
+// Toggle.js
+const Toggle = () => {
+  const toggle = useToggle();
+  return (
+    <div>
+      <div>{JSON.stringify(toggle)}</div>
+      <input type="checkbox" value={toggle} onChange={() => toggleSwitch()} />
+    </div>
+  );
+};
+
+// index.js
+const App = () => (
+  <Provider>
+    <Toggle />
+  </Provider>
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
