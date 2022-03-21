9594bcaaecc1204d0b183dd7411e30cb926d322f
Matthias Courville <matthias@genui.com>
2022-03-21 13:47:10 -0700

    Added support for  createStore to accept a reducer function as the first argument

--- src/createStore.js
+++ src/createStore.js
@@ -1,18 +1,36 @@
 const { createStore: reduxCreateStore, combineReducers } = require('redux')
 
-module.exports = (initialReducers = {}, ...args) => {
-  if (typeof initialReducers !== "object") {
-    console.error({initialReducers, args})
-    throw new Error("initialReducers should be an object suitable to be passed to combineReducers")
+module.exports = (initialReducer = {}, ...args) => {
+  let reducers
+  let combinedReducer
+  let rootReducer
+
+  if (typeof initialReducer !== "object" && typeof initialReducer !== "function") {
+    console.error({initialReducer, args})
+    throw new Error("initialReducer should be a reducing function (\"uncombined\") or an object suitable to be passed to combineReducers")
+  } else if (typeof initialReducer === "function") {
+    if (initialReducer.name === "combination") {
+      console.error({initialReducer, args})
+      throw new Error("initialReducer should be a reducing function (\"uncombined\") or an object suitable to be passed to combineReducers")
+    } else {
+      reducers = {_stub_: (s) => s || 0}
+      rootReducer = (state, action) => {
+        const intermediateState = combinedReducer(state, action)
+        return initialReducer(intermediateState, action)
+      }
+    }
+  } else {
+    reducers = {...initialReducer, _stub_: (s) => s || 0}
   }
 
-  const reducers = {...initialReducers, _stub_: (s) => s || 0}
-  const store = reduxCreateStore(combineReducers(reducers), ...args)
+  combinedReducer = combineReducers(reducers);
+  const store = reduxCreateStore(rootReducer || combinedReducer, ...args)
 
   store.injectReducer = (key, reducer) => {
     if (reducers[key]) console.warn(`injectReducer: replacing reducer for key '${key}'`);
     reducers[key] = reducer
-    store.replaceReducer(combineReducers(reducers))
+    combinedReducer = combineReducers(reducers);
+    store.replaceReducer(rootReducer || combinedReducer)
   }
 
   return store
--- src/tests/createStore.test.js
+++ src/tests/createStore.test.js
@@ -1,4 +1,47 @@
-import { getStore } from "../index";
+import renderer from 'react-test-renderer';
+import { Provider, setStore, getStore, createStore, createReduxModule } from "../index";
+
+describe("support default createStore API", () => {
+
+  const reducer = (state = { count: 0 }, action) => {
+      switch (action.type) {
+        case 'INCREMENT_COUNT':
+          return {...state, count: (state.count || 0) + 1}
+        default:
+          return state
+    }
+  }
+
+  const store = setStore(createStore(reducer))
+
+  const [useCount, {incrementCount}] = createReduxModule('count', 0, {
+    incrementCount: (state) => state + 1,
+  })
+
+  const [useMessage, {setMessage}] = createReduxModule('message', 'Hello!', {
+    setMessage: (state, message) => message,
+  })
+
+  it("initialize state", () => {
+    expect(store.getState().count).toEqual(0);
+    expect(store.getState().message).toEqual('Hello!');
+  })
+
+  it("increment count with hook", () => {
+    renderer.act(() => {incrementCount()})
+    expect(store.getState().count).toEqual(1);
+  })
+
+  it("increment count with dispatch", () => {
+    renderer.act(() => {store.dispatch({type: 'INCREMENT_COUNT'})})
+    expect(store.getState().count).toEqual(2);
+  })
+
+  it("set slice which is not part of initial state", () => {
+    renderer.act(() => {setMessage('Goodbye!')})
+    expect(store.getState().message).toEqual('Goodbye!');
+  })
+});
 
 it("injectReducer key must be unique", () => {
   getStore().injectReducer("myKey", () => 1);
