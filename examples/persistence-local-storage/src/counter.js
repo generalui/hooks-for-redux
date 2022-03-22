bd3f75a59992da1929a503be632de3374b8c4ed6
Vanessa Nunez <vanessa.cnr97@hotmail.com>
2022-03-22 16:56:20 -0700

    📝 add comments to persist data in local storage

--- examples/persistence-local-storage/src/counter.js
+++ examples/persistence-local-storage/src/counter.js
@@ -2,8 +2,15 @@ import { createReduxModule } from "hooks-for-redux";
 
 const STORE_NAME = "counter";
 
+/**
+ * This will check for the localStorage Item for initial state.
+ * If it doesn't exist, it will be 0.
+ */
 const initialState = parseInt(localStorage.getItem(STORE_NAME)) || 0;
 
+/**
+ * createReduxModule returns a virtualStore for the current Redux Store
+ */
 export const [
   useCounter,
   { increment, decrement, reset },
@@ -18,6 +25,10 @@ export const clearLocalStorageCounter = () => {
   localStorage.removeItem(STORE_NAME);
 };
 
+/**
+ * Subscribe to the virtualStore and update local storage
+ * when update are detected
+ */
 counterVirtualStore.subscribe((state) => {
   localStorage.setItem(STORE_NAME, state);
 });
