a938b238bee1a0a5597ec045bc679549602d79e0
Matthias Courville <matthias@genui.com>
2022-03-24 07:40:24 -0700

    Improved createStore performances

--- src/createStore.js
+++ src/createStore.js
@@ -1,16 +1,10 @@
 const { createStore: reduxCreateStore, combineReducers } = require('redux')
 
-const getRootReducerSlices = (state, reducers, initialReducer) => {
-  const initialSlices = initialReducer ? initialReducer({}, '') : {}
-  const initialKeys = Object.keys(initialSlices)
-  const knownKeys = Object.keys(reducers)
-  const slices = {
-    known: {},
-    unknown: {}
-  }
+const getStateSlices = (state, reducerKeys, initialKeys = []) => {
+  const slices = { known: {}, unknown: {} }
 
-  for (const key of Object.keys(state)) {
-    if (knownKeys.includes(key)) {
+  Object.keys(state).forEach((key) => {
+    if (reducerKeys.includes(key)) {
       slices.known[key] = state[key]
     } else {
       slices.unknown[key] = state[key]
@@ -19,49 +13,65 @@ const getRootReducerSlices = (state, reducers, initialReducer) => {
     if (initialKeys.includes(key)) {
       slices.unknown[key] = state[key]
     }
-  }
+  })
    
   return slices
 }
 
 module.exports = (initialReducer = {}, ...args) => {
+  let store
   let reducers
-  let rootReducer
   
+  // Passing an object of reducers is preferable for performances
   if (typeof initialReducer === "object") {
     reducers = {...initialReducer, _stub_: (s) => s || 0}
-  } else if (typeof initialReducer === "function") {
+    store = reduxCreateStore(combineReducers(reducers))
+    store.injectReducer = (key, reducer) => {
+      if (reducers[key]) console.warn(`injectReducer: replacing reducer for key '${key}'`)
+      reducers[key] = reducer
+      store.replaceReducer(combineReducers(reducers))
+    }
+  } 
+  // Support for default redux API (single or combined reducer)
+  else if (typeof initialReducer === "function") {
+    const initialKeys = Object.keys(initialReducer(undefined, '') || {})
     reducers = {_stub_: (s) => s || 0}
+    let reducerKeys = Object.keys(reducers)
+    let combinedReducer = combineReducers(reducers)
+    let rootReducer
+
+    // Combined reducer
     if (initialReducer.name === 'combination') {
       rootReducer = (state = {}, action) => {
-        const slices = getRootReducerSlices(state, reducers, initialReducer)
-        const intermediateState = combinedReducer(slices.known, action)
-        for (const key of Object.keys(intermediateState)) {
+        const slices = getStateSlices(state, reducerKeys, initialKeys)
+        const intermediate = combinedReducer(slices.known, action)
+        Object.keys(intermediate).forEach((key) => {
           if (slices.unknown.hasOwnProperty(key)) {
-            slices.unknown[key] = intermediateState[key]
+            slices.unknown[key] = intermediate[key]
           }
-        }
-        return {...intermediateState, ...initialReducer(slices.unknown, action)}
+        })
+        return {intermediate, ...initialReducer(slices.unknown, action)}
       }
-    } else {
+    } 
+    // Single reducer
+    else {
       rootReducer = (state = {}, action) => {
-        const slices = getRootReducerSlices(state, reducers)
-        const intermediateState = combinedReducer(slices.known, action)
-        return initialReducer({...state, ...intermediateState}, action)
+        const slices = getStateSlices(state, reducerKeys, initialKeys)
+        return initialReducer({...state, ...combinedReducer(slices.known, action)}, action)
       }
+    }
+
+    store = reduxCreateStore(rootReducer)
+    store.injectReducer = (key, reducer) => {
+      if (reducers[key]) console.warn(`injectReducer: replacing reducer for key '${key}'`)
+      reducers[key] = reducer
+      reducerKeys = Object.keys(reducers)
+      combinedReducer = combineReducers(reducers)
+      store.replaceReducer(rootReducer)
     }
   } else {
     console.error({initialReducer, args})
     throw new Error("initialReducer should be an object suitable to be passed to combineReducers or a reducing function (\"uncombined\")")
-  }
-
-  let combinedReducer = combineReducers(reducers)
-  const store = reduxCreateStore(rootReducer || combinedReducer)
-  store.injectReducer = (key, reducer) => {
-    if (reducers[key]) console.warn(`injectReducer: replacing reducer for key '${key}'`)
-    reducers[key] = reducer
-    combinedReducer = combineReducers(reducers)
-    store.replaceReducer(rootReducer || combinedReducer)
   }
 
   return store
